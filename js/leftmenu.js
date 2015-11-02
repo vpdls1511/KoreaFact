23ed87787885784f219674426d8148eefad93771
Kim HyeongMin <vpdls1511@gmail.com>
2015-11-02 18:05:19 +0900

    Frist commit

---
+++ js/leftmenu.js
@@ -0,0 +1,25 @@
+( function( $ ) {
+$( document ).ready(function() {
+$('#cssmenu ul ul li:odd').addClass('odd');
+$('#cssmenu ul ul li:even').addClass('even');
+$('#cssmenu > ul > li > a').click(function() {
+  var checkElement = $(this).next();
+  if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+    $(this).closest('li').removeClass('active');
+    checkElement.slideUp('normal');
+  }
+  if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+    checkElement.slideDown('normal');
+  }
+  if($(this).closest('li').find('ul').children().length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+});
+});
+} )( jQuery );
+
+function regis(){
+    window.location.href = '../register.html';
+}
